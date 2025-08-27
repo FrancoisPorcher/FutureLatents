64b534a9ad41ec044469a6c8642af896aa9e360e
francoisporher <francoisporcher@meta.com>
2025-08-27 16:07:57 +0000

    added device

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from transformers import AutoModel, AutoVideoProcessor
 
 from datasets.kinetics_400 import Kinetics400
 from utils.parser import create_parser
+import torch
 
 
 def load_config(path: Path) -> dict:
@@ -30,9 +31,13 @@ def main() -> None:
 
     config = load_config(Path(args.config_path))
     dataset = Kinetics400(config)
-
-    model = AutoModel.from_pretrained(config["backbone"]["hf_repo"]).eval()
+    breakpoint()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    breakpoint()
+    model = AutoModel.from_pretrained(config["backbone"]["hf_repo"]).to(device).eval()
     processor = AutoVideoProcessor.from_pretrained(config["backbone"]["hf_repo"])
+    
+    breakpoint()
     
     # try to get sample
     sample = dataset[10]
