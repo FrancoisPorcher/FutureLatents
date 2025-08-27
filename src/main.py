2770fb073ea77709ae90580634bdfe06b9ed8d72
François Porcher <93766133+FrancoisPorcher@users.noreply.github.com>
2025-08-27 16:32:10 +0200

    Implement Kinetics400 dataset and use in main

--- src/main.py
+++ src/main.py
@@ -1,3 +1,8 @@
+from pathlib import Path
+
+import yaml
+
+from datasets.kinetics_400 import Kinetics400
 from utils.parser import create_parser
 
 
@@ -5,7 +10,14 @@ def main() -> None:
     """Entry point for the FutureLatents application."""
     parser = create_parser()
     args = parser.parse_args()
-    breakpoint()
+
+    with open(Path(args.config_path)) as f:
+        config = yaml.safe_load(f)
+
+    csv_path = config["datasets"]["kinetics_400"]["paths"]["csv"]
+    dataset = Kinetics400(csv_path)
+
+    _ = len(dataset)  # simple usage to avoid unused variable warnings
 
 
 if __name__ == "__main__":
